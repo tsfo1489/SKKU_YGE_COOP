bd36c14335f471f97ddfaee1beefd8ea6478a482
tsfo1489 <tsfo1489@gmail.com>
2022-01-10 10:43:48 +0900

    fix: 페이스북 크롤러 headless 모드 해제

--- web/crawler/scrapy/YG_Crawler/spiders/FBSpider.py
+++ web/crawler/scrapy/YG_Crawler/spiders/FBSpider.py
@@ -81,7 +81,7 @@ class FBSpider(scrapy.Spider):
         options = webdriver.ChromeOptions()
         options.add_argument('window-size=1280,720')
         options.add_argument('loglevel=3')
-        # options.add_argument('headless')
+        options.add_argument('headless')
         options.add_argument('no-sandbox')
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         self.driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
