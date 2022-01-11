1fc8f48d939fc986280d96d9f7e109fb993acdfd
tsfo1489 <tsfo1489@gmail.com>
2022-01-11 12:01:54 +0900

    feat: 크롤러 파이프라인에 버퍼 추가하여 속도 향상

--- web/crawler/scrapy/YG_Crawler/pipelines.py
+++ web/crawler/scrapy/YG_Crawler/pipelines.py
@@ -5,14 +5,25 @@ from .apikey import *
 from scrapy.exceptions import DropItem
 
 class MongoDBPipelines:
+    BUF_SIZE = 300
     def __init__(self) -> None:
         super().__init__()
         connection = pymongo.MongoClient(
             MONGO_ADDR,
             MONGO_PORT
         )
-        self.db = connection['crawling_tuto']
-        
+        self.db = connection[MONGO_DB]
+        self.buffer = {}
     def process_item(self, item, spider):
-        self.db[item.target].insert_one(dict(item))
+        if item.target not in self.buffer:
+            self.buffer[item.target] = []
+        self.buffer[item.target].append(dict(item))
+        
+        if len(self.buffer[item.target]) == self.BUF_SIZE:
+            self.db[item.target].insert_many(self.buffer[item.target])
+            del self.buffer[item.target]
         return item
+    
+    def close_spider(self, spider):
+        for key in self.buffer:
+            self.db[key].insert_many(self.buffer[key])
