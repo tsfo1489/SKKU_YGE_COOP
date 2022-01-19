328b7667c12506946cb78f24e4f5fc05372dda86
tsfo1489 <tsfo1489@gmail.com>
2022-01-19 10:21:53 +0900

    feat: SSH 터널 기능 추가

--- web/crawler/scrapy/YG_Crawler/pipelines.py
+++ web/crawler/scrapy/YG_Crawler/pipelines.py
@@ -1,17 +1,24 @@
 import pymongo
-import scrapy.pipelines as pipelines
+import sshtunnel
 
 from .apikey import *
-from scrapy.exceptions import DropItem
 
 class MongoDBPipelines:
     BUF_SIZE = 300
     def __init__(self) -> None:
         super().__init__()
-        connection = pymongo.MongoClient(
-            MONGO_ADDR,
-            MONGO_PORT
-        )
+        if SSH_ENABLE:
+            self.tunnel = sshtunnel.SSHTunnelForwarder(
+                (SSH_HOST, SSH_PORT),
+                ssh_username=SSH_USER,
+                ssh_password=SSH_PSWD,
+                remote_bind_address=('127.0.0.1', 27017),
+                local_bind_address=('0.0.0.0', MONGO_PORT)
+            )
+            self.tunnel.start()
+            connection = pymongo.MongoClient(f'mongodb://{MONGO_USER}:{MONGO_PSWD}@127.0.0.1:{MONGO_PORT}')
+        else:
+            connection = pymongo.MongoClient(f'mongodb://{MONGO_USER}:{MONGO_PSWD}@{MONGO_ADDR}:{MONGO_PORT}')
         self.db = connection[MONGO_DB]
         self.buffer = {}
     def process_item(self, item, spider):
@@ -27,3 +34,5 @@ class MongoDBPipelines:
     def close_spider(self, spider):
         for key in self.buffer:
             self.db[key].insert_many(self.buffer[key])
+        if SSH_ENABLE:
+            self.tunnel.stop()
